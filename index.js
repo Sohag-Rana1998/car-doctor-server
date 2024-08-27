03266a83677ec8a9a859d26fa3f2af10d201e8e9
sohagislam1998 <sohagislampg1998@gmail.com>
2024-08-27 17:34:13 +0600

    update

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://car-doctor-client-10fa7.web.app",
+      "https://car-doctor-client-10fa7.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iulixph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,33 +28,133 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+const logger = async (req, res, next) => {
+  console.log("called:", req.host, req.originalUrl);
+  next();
+};
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies.token;
+  console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "not authorized" });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: "not authorized" });
+    }
+    console.log(decoded);
+    req.user = decoded;
+    next();
+  });
+};
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production" ? true : false,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
+    const servicesCollection = client.db("carDoctor").collection("services");
+    const OrdersCollection = client.db("carDoctor").collection("orders");
 
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+      const token = await jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
 
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log(req.body);
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
 
+      const options = {
+        projection: { _id: 0, title: 1, img: 1, price: 1, service_id: 1 },
+      };
+      const result = await servicesCollection.findOne(query, options);
+      res.send(result);
+    });
 
+    app.get("/orders", logger, verifyToken, async (req, res) => {
+      console.log(req.query?.email);
+      console.log(req.user);
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "Forbidden excess" });
+      }
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await OrdersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      console.log(order);
+      const result = await OrdersCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedOrder = req.body;
+      const updatedDoc = {
+        $set: {
+          status: updatedOrder.status,
+        },
+      };
+      const result = await OrdersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await OrdersCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
+    // Ensur that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Car doctor is running')
-})
+app.get("/", (req, res) => {
+  res.send("Car doctor is running");
+});
 
 app.listen(port, () => {
   console.log(`Car doctor is running on port ${port}`);
-})
+});
